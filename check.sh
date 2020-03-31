e726848d149ee014202c66170515bab1cfd2472e
absonggit <37949210+absonggit@users.noreply.github.com>
2020-03-31 19:42:52 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -97,7 +97,7 @@ root_file_check () {
     echo
     echo "【文件系统24小时内的文件修改】"
     echo "------------------------------------"
-    find / ! -path "/var/lib/filebeat/*" ! -path "/var/run/*" ! -path "/tmp/sess*" ! -path "/usr/local/php/var/log/*" ! -path "/home/wwwlogs/*" ! -path "/home/wwwroot/*" ! -path "/var/log/*" ! -path "/proc/32737" ! -path "/proc/*" ! -path "/sys/*" -type f -mtime 0
+    find / ! -path "/usr/local/share/aliyun-assist/" ! -path "/var/lib/filebeat/*" ! -path "/var/run/*" ! -path "/tmp/sess*" ! -path "/usr/local/php/var/log/*" ! -path "/home/wwwlogs/*" ! -path "/home/wwwroot/*" ! -path "/var/log/*" ! -path "/proc/32737" ! -path "/proc/*" ! -path "/sys/*" -type f -mtime 0
 }
 ip_check
 netstat_check
