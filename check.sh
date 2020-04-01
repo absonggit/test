e2e133182d2b7fde266b729c8ee40cdfca1e4e7b
absonggit <37949210+absonggit@users.noreply.github.com>
2020-04-01 19:43:54 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -97,7 +97,7 @@ root_file_check () {
     echo
     echo "【文件系统24小时内的文件修改】"
     echo "------------------------------------"
-    find / ! -path "/proc/28743" ! -path "/proc/26595" ! -path "/proc/19724" ! -path "/proc/32047" ! -path "/proc/32065" ! -path "/proc/32523" ! -path "/proc/32526" ! -path "/run/systemd/*" ! -path "/var/cache/man/*" ! -path "/usr/local/aegis/*" ! -path "/usr/local/share/aliyun-assist/*" ! -path "/var/lib/filebeat/*" ! -path "/var/run/*" ! -path "/tmp/sess*" ! -path "/usr/local/php/var/log/*" ! -path "/home/wwwlogs/*" ! -path "/home/wwwroot/*" ! -path "/var/log/*" ! -path "/proc/32737" ! -path "/proc/663" ! -path "/proc/12100" ! -path "/proc/*" ! -path "/sys/*" -type f -mtime 0
+    find / ! -path "/proc/15452" ! -path "/proc/28743" ! -path "/proc/26595" ! -path "/proc/19724" ! -path "/proc/32047" ! -path "/proc/32065" ! -path "/proc/32523" ! -path "/proc/32526" ! -path "/run/systemd/*" ! -path "/var/cache/man/*" ! -path "/usr/local/aegis/*" ! -path "/usr/local/share/aliyun-assist/*" ! -path "/var/lib/filebeat/*" ! -path "/var/run/*" ! -path "/tmp/sess*" ! -path "/usr/local/php/var/log/*" ! -path "/home/wwwlogs/*" ! -path "/home/wwwroot/*" ! -path "/var/log/*" ! -path "/proc/32737" ! -path "/proc/663" ! -path "/proc/12100" ! -path "/proc/*" ! -path "/sys/*" -type f -mtime 0
 }
 ip_check
 netstat_check
