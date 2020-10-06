972c70eaae927313dc71c2dd4400c3ccd5233574
absonggit <37949210+absonggit@users.noreply.github.com>
2020-10-07 01:55:12 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 nfs="1.1.1.1:/home/nfs/data/app"
+change_file=($(rsync --dry-run -rcnC --out-format="%n" ./ $nfs --exclude ".git"))
 
-change_file=($(rsync --dry-run -rcnC --out-format="%n" ./ $nfs \
---exclude ".git" \))
 for file in ${change_file[*]}
 do
     rsync -av $file  $nfs/$file
